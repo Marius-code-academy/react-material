4dd5bcfc9c9c53c00824f9f5d3640d358303eb58
Marius Matulevicius <marius.matulevicius@twoday.com>
2023-10-16 19:31:33 +0300

    paskaita 6

---
+++ Paskaita6/paskaita6/src/components/Test/Test.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+
+export default function Test() {
+  const [counter, setCounter] = useState(0)
+  const [counter2, setCounter2] = useState(0)
+
+  // useEffect(()=> {
+  //   console.log('Vidus useEffect');
+  // }, [counter, counter2])
+
+  useEffect(()=> {
+    console.log('Pasikeite counter 2');
+  }, [counter2])
+
+  useEffect(()=> {
+    const interval = setInterval(()=> {
+      setCounter((prev) => prev + 1);
+    }, 1000)
+
+    return ()=> {
+      clearInterval(interval);
+      console.log('Issitryne');
+    }
+  }, [])
+
+  return (
+    <>
+      <h2>{counter}</h2>
+      <h2>{counter2}</h2>
+      <button onClick={()=> setCounter((prev) => prev + 1)}>Add one</button>
+      <button onClick={()=> setCounter2((prev) => prev + 1)}>Add one to second</button>
+    </>
+  )
+}
