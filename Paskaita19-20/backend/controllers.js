0cd48359d2a09374b2b924c530660e6507d29ffa
Marius Matulevicius <marius.matulevicius@twoday.com>
2023-11-07 20:12:17 +0200

    paskaita 19

--- Paskaita19-20/backend/controllers.js
+++ Paskaita19-20/backend/controllers.js
@@ -1,5 +1,11 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import User from "./models/User.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 export async function register(req, res) {
   const { username, password } = req.body;
@@ -20,5 +26,37 @@ export async function register(req, res) {
 }
 
 export async function login(req, res) {
-  res.json({ message: "Login" });
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "username and password required" });
+  }
+
+  const user = await User.findOne({ username });
+
+  if (!user) {
+    return res.status(400).json({ message: "user not found" });
+  }
+
+  const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordCorrect) {
+    return res.status(400).json({ message: "Invalid password" });
+  }
+
+  const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: "1d" });
+
+  res.json({ token });
+}
+
+export async function getUsers(req, res) {
+  try {
+    const users = await User.find({});
+
+    console.log(req.userId);
+
+    res.status(200).json({ users });
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
 }
--- Paskaita19-20/backend/router.js
+++ Paskaita19-20/backend/router.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { login, register } from "./controllers.js";
+import { getUsers, login, register } from "./controllers.js";
+import { validateJwt } from "./middleware.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 
 router.post("/login", login);
+
+router.get("/users", validateJwt, getUsers);
+
+// router.get("/users/:id", validateJwt, getUsers);
 
 export default router;
